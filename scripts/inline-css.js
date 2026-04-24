c30d6259349fd1221c1540c8fd325e1872e3913e
Akansh Saxena <akanshsaxena@Mac.lan>
2026-04-24 22:59:51 +0530

    feat: [NFRONT-3434] loading and decoding images fixed on shopping page

--- scripts/inline-css.js
+++ scripts/inline-css.js
@@ -2,36 +2,39 @@ var fs = require('fs');
 var path = require('path');
 
 var outDir = path.join(__dirname, '..', 'out');
+var nrPath = path.join(outDir, 'newrelic.js');
+var nrScript = '';
 
-function inlineCss(htmlPath) {
+if (fs.existsSync(nrPath)) {
+  nrScript = '<script>' + fs.readFileSync(nrPath, 'utf8') + '<\/script>';
+}
+
+function process(htmlPath) {
   var html = fs.readFileSync(htmlPath, 'utf8');
-  var count = 0;
 
+  // Inline CSS
   html = html.replace(
     /<link rel="stylesheet" href="(\/_next\/static\/css\/[^"]+\.css)" data-precedence="next"\/>/g,
     function(match, cssHref) {
       var cssPath = path.join(outDir, cssHref);
-      var exists = fs.existsSync(cssPath);
-      count++;
-      console.log('  [' + path.basename(htmlPath) + '] #' + count + ' ' + cssHref + ' exists=' + exists);
-      if (exists) {
-        var css = fs.readFileSync(cssPath, 'utf8');
-        return '<style>' + css + '</style>';
+      if (fs.existsSync(cssPath)) {
+        return '<style>' + fs.readFileSync(cssPath, 'utf8') + '</style>';
       }
       return match;
     }
   );
 
+  // Remove RSC CSS hints
   html = html.replace(/\d+:HL\[\\"(\/_next\/static\/css\/[^"]+\.css)\\",\\"style\\"\]\\n/g, '');
 
+  // Inject New Relic script right after <head>
+  if (nrScript) {
+    html = html.replace('<head>', '<head>' + nrScript);
+  }
+
   fs.writeFileSync(htmlPath, html);
-  return count;
 }
 
 var htmlFiles = fs.readdirSync(outDir).filter(function(f) { return f.endsWith('.html'); });
-var total = 0;
-htmlFiles.forEach(function(f) {
-  var c = inlineCss(path.join(outDir, f));
-  total += c;
-});
-console.log('Replaced ' + total + ' links across ' + htmlFiles.length + ' HTML files');
+htmlFiles.forEach(function(f) { process(path.join(outDir, f)); });
+console.log('Processed ' + htmlFiles.length + ' HTML files' + (nrScript ? ' (with New Relic)' : ''));
